3c5cf54974da2af5ba66a80cb1459d3c0e0d8523
RuanCunha <ruanvcunha@hotmail.com>
2021-07-26 03:55:34 -0300

    Requisitos 1 a 7 finalizados

--- src/zoo.js
+++ src/zoo.js
@@ -1,32 +1,55 @@
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  // Ponto para primeiro commit
+const { species } = data;
+const { employees } = data;
+
+function getSpeciesByIds(...ids) {
+  // if (ids === undefined) return [];
+  const result = species.filter((specie) => ids.includes(specie.id));
+  return result;
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findAnimal = species.find((specie) => specie.name === animal);
+  const result = findAnimal.residents.every((anim) => anim.age >= age);
+  return result;
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  const result = employees.find((employee) => employeeName === employee.firstName || employeeName === employee.lastName);
+  return result;
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
   // seu código aqui
+  const manager = employees.find((employee) => employee.managers.includes(id));
+  // console.log(manager);
+  if (manager) return true;
+  return false;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers =[], responsibleFor =[]) {
+  const result = data.employees.push({ id, firstName, lastName, managers, responsibleFor});
+  return result;
 }
 
-function countAnimals(species) {
-  // seu código aqui
+function countAnimals(speciesName) {
+  if (speciesName === undefined) {
+    const allAnimals = {};
+    data.species.forEach((element) => {
+      allAnimals[element.name] = element.residents.length;
+    });
+    return allAnimals;
+  } else {
+    const findAnimal = data.species.find((specie) => specie.name === speciesName);
+    const result = findAnimal.residents.length;
+    return result;
+  }
 }
 
 function calculateEntry(entrants) {
